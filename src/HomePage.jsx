6faac79b3459e46ff1858de4605362ac73dcedd7
Bita Zamani <bita.zamany@gmail.com>
2025-08-23 02:18:27 +0330

    minor ui changes

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,6 +4,28 @@ import Button from "./components/UI/button";
 import Card from "./components/UI/card/card";
 
 const HomePage = () => {
+  const number2 = (
+    <svg
+      fill="#fddc3e"
+      viewBox="0 0 256 256"
+      width="80"
+      height="80"
+      id="Flat"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#fddc3e"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path d="M208,32H48A16.01833,16.01833,0,0,0,32,48V208a16.01833,16.01833,0,0,0,16,16H208a16.01833,16.01833,0,0,0,16-16V48A16.01833,16.01833,0,0,0,208,32ZM152,167.99414a8,8,0,0,1,0,16H104.31738c-.10644.0044-.21191.00635-.31836.00635a8.00251,8.00251,0,0,1-6.30078-12.93164L141.37012,112.793a16.0039,16.0039,0,1,0-28.11621-15.01856A8,8,0,1,1,98.51758,91.542a32.00411,32.00411,0,1,1,56.01269,30.35547c-.07324.10791-.14843.21436-.22754.31836l-34.30566,45.77832Z"></path>{" "}
+      </g>
+    </svg>
+  );
   const bomb = (
     <svg
       viewBox="0 0 16 16"
@@ -44,16 +66,7 @@ const HomePage = () => {
           <h2 className="text-center text-3xl my-7 ">Ready To Play?</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-14 max-w-[400px] sm:max-w-[800px] mx-auto">
             <NavLink to={"/2048"}>
-              <Card
-                icon={
-                  <img
-                    src="src/assets/2048.webp"
-                    className="rounded-4xl w-36"
-                  />
-                }
-              >
-                2048
-              </Card>
+              <Card icon={number2}>2048</Card>
             </NavLink>
             <NavLink to={"/MineSweeper"}>
               <Card icon={bomb}>
@@ -62,6 +75,12 @@ const HomePage = () => {
             </NavLink>
           </div>
         </section>
+      </div>
+      <div className="absolute bottom-0 left-0 -translate-y-1/2 w-full text-center text-orange-950">
+        Created By{" "}
+        <a href="https://github.com/BitaZamani" className="text-orange-400">
+          Bita Zamani
+        </a>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,5 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <div className="sticky bottom-0 text-center text-orange-950">
-      Created By{" "}
-      <a href="https://github.com/BitaZamani" className="text-orange-400">
-        Bita Zamani
-      </a>
-    </div>
   </StrictMode>
 );
