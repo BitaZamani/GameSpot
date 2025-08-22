e59b3debd4d2a0b1c9b2a860d8f44563a132584b
Bita Zamani <bita.zamany@gmail.com>
2025-08-23 02:20:44 +0330

    minor ui changes

--- src/components/UI/gameOver.jsx
+++ src/components/UI/gameOver.jsx
@@ -29,7 +29,7 @@ const GameOver = ({
               <Button
                 handleclick={handleclick}
                 text={"Try Again"}
-                variant={varinat}
+                variant={page}
               />
             </>
           )}
